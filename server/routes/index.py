b8823a585712d371acbb9e9d0472d9cffec5849e
Spencer Krum <nibz@spencerkrum.com>
2020-04-01 22:14:36 +0000

    Add /fib route to do extra work

--- server/routes/index.py
+++ server/routes/index.py
@@ -1,9 +1,27 @@
 from server import app
 from flask import render_template
 
+def calc_fib(i):
+    if i is 0:
+        return 0
+    
+    total = 1
+    prev_total = 0
+    for i in range(i-1):
+        temp = total + prev_total
+        prev_total = total
+        total = temp
+
+    return(total)
+
 @app.route('/')
 def hello_world():
     return app.send_static_file('index.html')
+
+@app.route('/fib/<val>')
+def fib(val):
+    var = calc_fib(int(val))
+    return "The value is {}\n".format(str(var))
 
 @app.errorhandler(404)
 @app.route("/error404")
